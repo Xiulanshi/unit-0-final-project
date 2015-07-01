34cb40024a9eb37a8b2339d2cdc8c8aaf2531dd1
Xiulan <xiulanshi2013@gmail.com>
2015-07-01 11:19:28 -0400

    Update main.m

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -2,11 +2,48 @@
 //  main.m
 //  TodoList
 //
-//  Created by Michael Kavouras on 6/25/15.
-//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//  Created by Xiulan Shi on 07/01/15.
+//  Copyright (c) 2015 Xiulan Shi. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+// forward class declaration
+@class Item;
+@class List;
+
+@interface Item : NSObject
+- (void)setName:(NSString *)name;
+- (NSString *)name;
+- (void)setPriority;
+- (int)priority;
+- (BOOL)isComplete;
+- (void)setCompletionDate;
+@end
+
+@implementation Item {
+ NSString *_name;
+ int _priority;
+ BOOL _isComplete;
+ NSDate *_completionDate;
+}
+- (void)setName:(NSString *)name {
+    _name = name;
+}
+- (NSString *)name {
+    return _name;
+}
+- (void)setPriority {
+    _priority = priority;
+}
+- (int)priority {
+    return _priority;
+}
+- (BOOL)isComplete {
+}
+- (void)setCompletionDate {
+}
+
+@end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
